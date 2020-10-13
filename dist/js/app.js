7e903f061be783d3451ab4e284038340364472e1
morelmiles <morelmiles@gmail.com>
2020-10-13 18:01:20 +0300

    Add the about page

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,9 @@
-app.js
+// TESTIMONIAL SLIDER
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    arrows: true
+});
